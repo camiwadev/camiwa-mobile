7f9e40fed7cc9abb005f3e44ad78494020b4cb08
root <root@localhost.localdomain>
2024-08-31 07:55:56 -0500

    address sync

--- src/app/components/ui/header/header.component.ts
+++ src/app/components/ui/header/header.component.ts
@@ -1,15 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { PocketAuthService } from '@app/services/auth-pocketbase.service';
 import { GlobalService } from '@app/services/global-service.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
 constructor(
+  public auth: PocketAuthService,
   public global:GlobalService
 ){}
 }
